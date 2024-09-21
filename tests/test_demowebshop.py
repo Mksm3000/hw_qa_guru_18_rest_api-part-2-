e1af552e32628c9bb27fad0bfdff3239e0e0af5b
Maksim Zosimov <mizo-y@yandex.ru>
2024-09-21 15:51:59 +0200

    добавлены исправления

---
+++ tests/test_demowebshop.py
@@ -0,0 +1,11 @@
+from pages.product_page import laptop, smartphone
+from pages.cart_page import cart
+import pytest
+
+
+@pytest.mark.parametrize('product', [laptop, smartphone], ids=['laptop','smartphone'])
+def test_adding_to_cart(product):
+    cart.add_product_to_cart(product)
+    cart.verify_product_in_cart(product)
+    # cart.remove_product_from_cart(product)
+
